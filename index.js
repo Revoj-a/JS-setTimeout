ee66b4d0f2ef4217264f6f0f62d4d96a80cf7148
Revoj-a <revojjoever@gmail.com>
2025-08-24 14:51:49 +0800

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-setTimeout(() => {
-  document.body.style.backgroundColor = "lightblue";
+// setTimeout(() => {
+//   document.body.style.backgroundColor = "lightblue";
+//   setTimeout(() => {
+//     document.body.style.backgroundColor = "lightgreen";
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = "lightcoral";
+//       setTimeout(() => {
+//         document.body.style.backgroundColor = "yellow";
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+const delayedColorChange = (newColor, delay, doNext) => {
   setTimeout(() => {
-    document.body.style.backgroundColor = "lightgreen";
-  }, 1000);
-}, 1000);
+    document.body.style.backgroundColor = newColor;
+    doNext && doNext();
+  }, delay);
+};
+
+delayedColorChange("red", 1000, () => {
+  delayedColorChange("orange", 1000, () => {
+    delayedColorChange("blue", 1000, () => {
+      delayedColorChange("green", 1000, () => {});
+    });
+  });
+});
